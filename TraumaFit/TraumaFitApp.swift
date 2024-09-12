e7617b1c1dd7ba32d5e75065d0c971deafb28dad
Zihuayang <zihuayang@csu.fullerton.edu>
2024-09-11 23:55:30 -0700

    Initial Commit

---
+++ TraumaFit/TraumaFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraumaFitApp.swift
+//  TraumaFit
+//
+//  Created by Jay Yang on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TraumaFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
